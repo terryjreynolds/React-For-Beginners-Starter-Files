64b5c72930ee90f35a5ba445f74ed19830546042
Terry Reynolds <terrancejreynolds@gmail.com>
2018-08-10 15:59:36 -0400

    vid 16 add to cart button uses func to set state of order object

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -24,6 +24,13 @@ class App extends React.Component {
             fishes: sampleFishes
         });
     };
+
+    addToOrder = (key) => {
+            const order = {...this.state.order};
+            order[key]= order[key] + 1 || 1;
+            this.setState({ order });
+        };
+    
   render() {
     return (
       <div className="catch-of-the-day">
@@ -31,7 +38,7 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market" />
           <ul className="fishes">
             { Object.keys(this.state.fishes).map(key => 
-            <Fish key={key} details={this.state.fishes[key]} />
+            <Fish key={key} details={this.state.fishes[key]} index={key} addToOrder={this.addToOrder} />
             )}
           </ul>
         </div>
